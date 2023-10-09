d136ec1c137ca46fe3a3f613ff1696ea5bf78abc
augusto <gtoborges@gmail.com>
2023-10-09 14:20:17 -0300

    add skills component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HeaderComponent from './components/HeaderComponent.vue'
 import ExperiencesComponent from './components/ExperiencesComponent.vue';
 import EducationComponent from './components/EducationComponent.vue'
 import ProjectsComponent from './components/ProjectsComponent.vue'
+import SkillsComponent from './components/SkillsComponent.vue'
 import resume_config from './assets/resume_config.json'
 
 const lang = ref('')
@@ -26,6 +27,7 @@ onBeforeMount(() => {
     <ExperiencesComponent v-if="resume_config.resume_sections['experiences']" :lang="lang"/>
     <EducationComponent v-if="resume_config.resume_sections['education']" :lang="lang"/>
     <ProjectsComponent v-if="resume_config.resume_sections['projects']" :lang="lang"/>
+    <SkillsComponent v-if="resume_config.resume_sections['projects']" :lang="lang"/>
     <footer class="page-footer">
       <p class="footer-line">Made by <a href="http://twitter.com/jglovier">@jglovier</a>. Fork me on <a href="https://github.com/jglovier/resume-template">GitHub</a>.</p>
     </footer>
---
+++ src/components/SkillsComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import skills from '../assets/skills.json'
+const props = defineProps(['lang'])
+</script>
+
+<template>
+  <section class="content-section">
+    <header class="section-header">
+      <h2>Skills</h2>
+    </header>
+    <div class="resume-item" v-for="skill in skills[props.lang]">
+      <h4 class="resume-item-details">{{ skill.name }}</h4>
+      <p class="resume-item-copy">{{ skill.description }}</p>
+    </div>
+  </section>
+</template>
